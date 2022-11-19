20bdb9b63863353c6a68b25e66ca32e26f6b08d1
Jake Ryu <jake.ryu@hotmail.co.uk>
2022-11-19 07:10:47 +0000

    Test captured variables

--- code/CSharpInDepthStudy/Chapter3/AnonymousTypeTests.cs
+++ code/CSharpInDepthStudy/Chapter3/AnonymousTypeTests.cs
@@ -1,9 +1,17 @@
 using Shouldly;
+using Xunit.Abstractions;
 
 namespace CSharpInDepthStudy.Chapter3;
 
 public class AnonymousTypeTests
 {
+    private readonly ITestOutputHelper output;
+
+    public AnonymousTypeTests(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+    
     [Fact]
     public void AnonymousType_WhenPropertiesAreSame_ShouldEqual()
     {
@@ -45,6 +53,25 @@ public class AnonymousTypeTests
 
         a = b;
     }
+
+    [Fact]
+    public void RunDemo()
+    {
+        var demo = new CapturedVariableDemo(this.output);
+        Action<string> action = demo.CreateAction("method argument");
+        action("lambda argument");
+    }
+    
+    [Fact]
+    public void RunDemo2()
+    {
+        var demo = new CapturedVariableDemo(this.output);
+        List<Action> actions = demo.CreateActions();
+        foreach (Action action in actions)
+        {
+            action();
+        }
+    }
 }
 
 public class Location
@@ -59,3 +86,43 @@ public class Location
     //     return X == compareTo.X && Y == compareTo.Y;
     // }
 }
+
+class CapturedVariableDemo
+{
+    private string instanceField = "instance field";
+    private readonly ITestOutputHelper output;
+
+    public CapturedVariableDemo(ITestOutputHelper output)
+    {
+        this.output = output;
+    }
+    public Action<string> CreateAction(string methodParameter)
+    {
+        string methodLocal = "method local";
+        string uncaptured = "uncaptured local";
+        Action<string> action = lambdaParameter =>
+        {
+            string lambdaLocal = "lambda local";
+            this.output.WriteLine("Instance field: {0}", instanceField);
+            this.output.WriteLine("Method parameter: {0}", methodParameter);
+            this.output.WriteLine("Method local: {0}", methodLocal);
+            this.output.WriteLine("Lambda parameter: {0}", lambdaParameter);
+            this.output.WriteLine("Lambda local: {0}", lambdaLocal);
+        };
+        methodLocal = "modified method local"; // caution!!!
+        return action;
+    }
+
+    public List<Action> CreateActions()
+    {
+        List<Action> actions = new List<Action>();
+        for (int i = 0; i < 5; i++)
+        {
+            string text = string.Format("message {0}", i);
+            actions.Add(() => output.WriteLine(text));
+        }
+        
+        return actions;
+    }
+}
+
